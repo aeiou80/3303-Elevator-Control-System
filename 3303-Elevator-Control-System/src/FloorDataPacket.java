b6697897cabfd1e73eb56161d92a103320c42299
Eric <ericvincent@cmail.carleton.ca>
2020-01-30 14:22:28 -0500

    Created an object used to store all the data from the floorsubsystem

---
+++ 3303-Elevator-Control-System/src/FloorDataPacket.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class FloorDataPacket {
+
+		private String time;
+		private String floor;
+		private String floorButton;
+		private String carButton;
+		
+		public FloorDataPacket(ArrayList<String> data) {
+			if(data.size() != 4) {
+				int difference = 4 - data.size();
+				for(int i = 0; i < difference; i++) {
+					data.add("");
+				}
+			}
+			this.time = data.get(0);
+			this.floor = data.get(1);
+			this.floorButton = data.get(2);
+			this.carButton =  data.get(3);
+			
+			
+		}
+		
+		public String toString() {
+			String value = "{" + this.time + "," + this.floor + "," + this.floorButton + "," + this.carButton + "}";
+			return value;
+		}
+	}
+
